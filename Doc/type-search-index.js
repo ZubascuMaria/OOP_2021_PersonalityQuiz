cf380c0d61a440f56f76df63265a37e391bb4b6c
ZubascuMaria <115020459+ZubascuMaria@users.noreply.github.com>
2023-01-09 19:07:12 +0200

    Add files via upload

---
+++ Doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Answer"},{"p":"<Unnamed>","l":"Question"},{"p":"<Unnamed>","l":"Quiz"},{"p":"<Unnamed>","l":"Result"},{"p":"<Unnamed>","l":"Rules"}];updateSearchResults();
